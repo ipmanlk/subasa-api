3af4b08f00ecbaa2a5216228b0bd257eb126a6d2
ipmanlk <io@navinda.xyz>
2020-04-03 17:59:38 +0530

    add: more tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,20 +2,28 @@ const { expect } = require('chai');
 const index = require("../index");
 
 describe('text correction', function () {
-    const text = "මථකය (ස්මෘතිය) යනු ජීවියෙකු තුළ තොරතුරු ගබඩා කිරීමට සහ ඉන්පසුව එම තොරතුරු ණැවත එලි දැක්වීමට ඇති හැකියාවයි.";
+    const texts = [
+        "මථකය (ස්මෘතිය) යනු ජීවියෙකු තුළ තොරතුරු ගබඩා කිරීමට සහ ඉන්පසුව එම තොරතුරු ණැවත එලි දැක්වීමට ඇති හැකියාවයි.",
+        "කොරෝනාවෛරසයේ බලපෑම ලොව පුරා වෙසෙන ශ්‍රී ලාංකිකයින්ට වඩාත් බරපතළ ලෙස සංවේදී වන්නේ, ඔවුන් මව්බිම නොවන වෙනත් රටක සිට මෙම තත්ත්වයට මුහුණ දෙමින් සිටින හෙයිනි.",
+        "දුකට පත් වැළපෙන්නන් මෘතදේහය වටා පැදකුණු කරමින් ගමන් ගන්නේ දේහය නරඹමිනි. පසුව මියගිය තැනැත්තාගේ පවුලේ සාමාජිකයන්ට අතට අත දී සිය ශෝකය පළ කරති."
+    ];
 
     it('should give a non empty corrected string', (done) => {
-        index.correct(text).then(data => {
-            expect(data.text).to.be.not.empty.string;
-            done();
-        });
+        for (let text of texts) {            
+            index.correct(text).then(data => {
+                expect(data.text).to.be.not.empty.string;
+            });
+        }
+        done();
     });
 
     it('should give an object with corrections', (done) => {
-        index.correct(text).then(data => {
-            expect(data.corrections).to.be.an("Object");
-            done();
-        });
+        for (let text of texts) {            
+            index.correct(text).then(data => {
+                expect(data.corrections).to.be.an("Object");
+            });
+        }
+        done();
     });
 
     it('should reject with an error when input text is not given', (done) => {
